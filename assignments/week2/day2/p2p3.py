97a8d65261f6ec928a11707f059626cde3eb65cc
Ralph Vente <venteralph@gmail.com>
2020-01-08 20:00:21 -0500

    add remaining p2p3 assig

--- assignments/week2/day2/p2p3.py
+++ assignments/week2/day2/p2p3.py
@@ -1,4 +1,16 @@
-# https://leetcode.com/problems/unique-number-of-occurrences/submissions/
+# https://leetcode.com/problems/jewels-and-stones/
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        sCount = collections.Counter(S)
+        jSet = set(J)
+        totGems = 0
+        for item in sCount:
+            if item in jSet:
+                totGems += sCount[item]
+
+        return totGems
+
+# https://leetcode.com/problems/unique-number-of-occurrences/
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
         
@@ -10,3 +22,22 @@ class Solution:
                 return False
             
         return True
+
+
+# https://leetcode.com/problems/word-pattern/
+class Solution:
+    def wordPattern(self, pattern: str, string: str) -> bool:
+        sList = string.split()
+        mapping = dict()
+        seen = set()
+        if len(sList) != len(pattern):
+            return False
+        for c,word in zip(pattern, sList):
+            if c not in mapping and word not in seen:
+                seen.add(word)
+                mapping[c] = word
+            else:
+                if c not in mapping or mapping[c] != word:
+                   return False
+        return True
+
