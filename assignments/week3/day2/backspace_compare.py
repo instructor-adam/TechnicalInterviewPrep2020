85b5ae2d8bbbf2522ea42a96f385d360482dfb73
Tyler <tptorres5@gmail.com>
2020-01-13 22:55:27 -0500

    Solution to backspace compare

---
+++ assignments/week3/day2/backspace_compare.py
@@ -0,0 +1,22 @@
+def backspaceCompare(self, S: str, T: str) -> bool:
+    stack_S = []
+    stack_T = []
+
+    for char in S:
+        if char == "#" and not stack_S:
+            print("WORKED")
+            continue
+        elif char == "#":
+            stack_S.pop()
+        else:
+            stack_S.append(char)
+
+    for char in T:
+        if char == "#" and not stack_T:
+            continue
+        elif char == "#":
+            print(char)
+            stack_T.pop()
+        else:
+            stack_T.append(char)
+    return True if stack_S == stack_T else False
